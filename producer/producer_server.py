430ae3a648631e843d5910c775ecd680cafe1efa
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2022-03-25 17:03:50 +0900

    WIP

--- producer/producer_server.py
+++ producer/producer_server.py
@@ -1,7 +1,8 @@
 from kafka import KafkaProducer
 import json
 import time
-
+import uuid
+import datetime
 
 class ProducerServer(KafkaProducer):
 
@@ -16,9 +17,13 @@ class ProducerServer(KafkaProducer):
         json_load = json.load(input_file)
         for v in json_load:
             message = self.dict_to_binary(v)
+            gen_id = str(uuid.uuid4())
+            print(f"uuid: {gen_id}")
             print(f"message: {message}")
-            #self.send(self.topic, v)
-            self.send(self.topic, message)
+
+            #temporary#self.send(self.topic, v)
+
+            #self.send(self.topic, message)
             time.sleep(1)
         #with open(self.input_file) as f:
         #    for line in f:
@@ -32,10 +37,21 @@ class ProducerServer(KafkaProducer):
         return json.dumps(json_dict).encode('utf-8')
 
 def main():
+    gen_id = str(uuid.uuid4())
+    print(f"uuid: {gen_id}")
+    dt_now = datetime.datetime.now()
+    print(f"dt_now: {dt_now}")
+
+    cert_folder = "."
     producer_server = ProducerServer("./police-department-calls-for-service.json", 
-                                     "police-department-calls-for-service", bootstrap_servers=['localhost:9092'],
+                                     #"police-department-calls-for-service", bootstrap_servers=['localhost:9092'],
+                                     "police-department-calls-for-service", bootstrap_servers=['kafka-test-yoshiyuki-e281.aivencloud.com:10779'],
                                      #value_serializer=lambda m: json.dumps(m).encode('ascii')
-                                     value_serializer=lambda x: json.dumps(x).encode('utf-8')
+                                     value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+                                     security_protocol="SSL",
+                                     ssl_cafile=cert_folder+'/ca.pem',
+                                     ssl_certfile=cert_folder+'/service.cert',
+                                     ssl_keyfile=cert_folder+'/service.key',
                                     )
     producer_server.generate_data()
     
